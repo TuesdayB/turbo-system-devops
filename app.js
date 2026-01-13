1d9dbd3fc74b36c7adab201a5c11e2f3cdaeddce
Brown <tbrown31@una.edu>
2026-01-13 13:11:18 -0600

    baby's first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+//app.js
+
+import express from 'express'
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Hello Express')
+})
+
+//starts the server
+app.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000')
+})
